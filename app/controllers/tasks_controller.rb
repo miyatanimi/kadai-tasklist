b3ed0a2901555b968b648d04c244c0aafbe960e6
miyata <mitanitani.tani@gmail.com>
2018-06-19 11:21:07 +0000

    DRY up

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show,:edit,:update,:destroy]
   
   def index
     @tasks = Task.all
@@ -21,16 +22,12 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = "タスク変更しました！"
       redirect_to @task
@@ -41,11 +38,16 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "削除しました"
     redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
   end
   
   def task_params
